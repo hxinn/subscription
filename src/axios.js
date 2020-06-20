489f7ca4690d1a718e4f3b73043d078cf5d3cff1
xhxin <445623551@qq.com>
2020-06-20 09:42:26 +0800

    update style

--- src/axios.js
+++ src/axios.js
@@ -1,28 +1,31 @@
-import axios from 'axios';
-import qs from 'qs';
-import {Message} from 'element-ui'
+import axios from "axios"
+import qs from "qs"
+import { Message } from "element-ui"
 
 // 创建axios实例
 const service = axios.create({
-    timeout: 30000 // 请求超时时间
+    timeout: 30000, // 请求超时时间
 })
 // 添加request拦截器
-service.interceptors.request.use(config => {
-    return config
-}, error => {
-    Promise.reject(error)
-})
+service.interceptors.request.use(
+    config => {
+        return config
+    },
+    error => {
+        Promise.reject(error)
+    }
+)
 // 添加respone拦截器
 service.interceptors.response.use(
     response => {
-        let res={};
-        res.status=response.status
-        res.data=response.data;
-        return res;
+        let res = {}
+        res.status = response.status
+        res.data = response.data
+        return res
     },
     error => {
-        if(error.response && error.response.status == 404){
-            Message.error("页面不存在");
+        if (error.response && error.response.status == 404) {
+            Message.error("页面不存在")
         }
         return Promise.reject(error.response)
     }
@@ -32,41 +35,38 @@ export function get(url, params = {}) {
     // params.t = new Date().getTime(); //get方法加一个时间参数,解决ie下可能缓存问题.
     return service({
         url: url,
-        method: 'get',
-        headers: {
-        },
-        params
+        method: "get",
+        headers: {},
+        params,
     })
 }
-
 
 //封装post请求
 export function post(url, data = {}) {
     //默认配置
-    let sendObject={
+    let sendObject = {
         url: url,
-        method: 'post',
+        method: "post",
         headers: {
-            'Content-Type':'application/json;charset=UTF-8'
+            "Content-Type": "application/json;charset=UTF-8",
         },
-        data:data
-    };
-    sendObject.data=JSON.stringify(data);
+        data: data,
+    }
+    sendObject.data = JSON.stringify(data)
     return service(sendObject)
 }
-
 
 //封装post请求
 export function filepost(url, data = {}) {
     //默认配置
-    let sendObject={
+    let sendObject = {
         url: url,
-        method: 'post',
+        method: "post",
         headers: {
-            "Content-Type": "multipart/form-data"
+            "Content-Type": "multipart/form-data",
         },
-        data:data
-    };
+        data: data,
+    }
     //sendObject.data=JSON.stringify(data);
     return service(sendObject)
 }
@@ -74,18 +74,16 @@ export function filepost(url, data = {}) {
 //封装post请求
 export function formpost(url, data = {}) {
     //默认配置
-    let sendObject={
+    let sendObject = {
         url: url,
-        method: 'post',
+        method: "post",
         headers: {
-            "Content-Type": "application/x-www-form-urlencoded"
+            "Content-Type": "application/x-www-form-urlencoded",
         },
-        data:qs.stringify(data)
-    };
+        data: qs.stringify(data),
+    }
     //sendObject.data=JSON.stringify(data);
     return service(sendObject)
 }
 
-export {
-    service
-}
+export { service }
--- src/views/post.vue
+++ src/views/post.vue
@@ -1,81 +1,106 @@
 <template>
-  <div>
-    <h2 class="rich_media_title">{{title}}</h2>
-    <div class="rich_media_subtitle">阿里技术&nbsp;昨天</div>
-    <div
-      class="contianer"
-      style="width: 100%; height: 100%;padding: 10px 16px 12px; padding: calc(10px + constant(safe-area-inset-top)) calc(16px + constant(safe-area-inset-right)) 12px calc(16px + constant(safe-area-inset-left)); padding: calc(10px + env(safe-area-inset-top)) calc(16px + env(safe-area-inset-right)) 12px calc(16px + env(safe-area-inset-left));"
-    >
-      <div id="output">
-      </div>
+    <div>
+        <h2 class="rich_media_title">{{ title }}</h2>
+        <div class="rich_media_subtitle">阿里技术&nbsp;昨天</div>
+        <div
+            class="contianer"
+            style="
+                width: 100%;
+                height: 100%;
+                padding: 10px 16px 12px;
+                padding: calc(10px + constant(safe-area-inset-top))
+                    calc(16px + constant(safe-area-inset-right)) 12px
+                    calc(16px + constant(safe-area-inset-left));
+                padding: calc(10px + env(safe-area-inset-top))
+                    calc(16px + env(safe-area-inset-right)) 12px
+                    calc(16px + env(safe-area-inset-left));
+            "
+        >
+            <div id="output"></div>
+        </div>
+        <el-row style="text-align: center;">
+            <el-button @click="openView('qlinks://to/news')"
+                >打开新闻列表</el-button
+            >
+            <el-button
+                @click="openView('qlinks://to/news?id=42223348285118')"
+                type="primary"
+                >打开新闻列表详情</el-button
+            ><br /><br />
+            <el-button @click="openView('qlinks://to/sign')" type="success"
+                >签到</el-button
+            >
+            <el-button @click="openView('qlinks://to/luckyWheel')" type="info"
+                >大转盘</el-button
+            ><br /><br />
+            <el-button @click="openView('qlinks://to/login')" type="warning"
+                >登陆</el-button
+            >
+            <el-button @click="openView('qlinks://to/bookmarks')" type="danger"
+                >书签</el-button
+            ><br /><br />
+        </el-row>
+        <el-row style="text-align: center;">
+            <el-button icon="el-icon-search" circle></el-button>
+            <el-button type="primary" icon="el-icon-edit" circle></el-button>
+            <el-button type="success" icon="el-icon-check" circle></el-button>
+            <el-button type="info" icon="el-icon-message" circle></el-button>
+            <el-button
+                type="warning"
+                icon="el-icon-star-off"
+                circle
+            ></el-button>
+            <el-button type="danger" icon="el-icon-delete" circle></el-button>
+        </el-row>
     </div>
-  <el-row style="text-align: center;">
-    <el-button @click="openView('qlinks://to/news')">打开新闻列表</el-button>
-    <el-button @click="openView('qlinks://to/news?id=42223348285118')" type="primary">打开新闻列表详情</el-button><br><br>
-    <el-button @click="openView('qlinks://to/sign')" type="success">签到</el-button>
-    <el-button @click="openView('qlinks://to/luckyWheel')" type="info">大转盘</el-button><br><br>
-    <el-button @click="openView('qlinks://to/login')" type="warning">登陆</el-button>
-    <el-button @click="openView('qlinks://to/bookmarks')" type="danger">书签</el-button><br><br>
-  </el-row>
-  <el-row style="text-align: center;">
-
-    <el-button icon="el-icon-search" circle></el-button>
-    <el-button  type="primary" icon="el-icon-edit" circle></el-button>
-    <el-button type="success" icon="el-icon-check" circle></el-button>
-    <el-button type="info" icon="el-icon-message" circle></el-button>
-    <el-button type="warning" icon="el-icon-star-off" circle></el-button>
-    <el-button type="danger" icon="el-icon-delete" circle></el-button>
-  </el-row>
-  </div>
-
 </template>
 
 <script>
-import "./../assets/css/app.css";
+import "./../assets/css/app.css"
 export default {
-    data(){
-      return{
-        title:"当猫做成手办后，网友：猫界又出新品种了？！"
-      }
+    data() {
+        return {
+            title: "当猫做成手办后，网友：猫界又出新品种了？！",
+        }
     },
     mounted() {
-      document.title = this.title;
-      console.log()
-      location.href="qlinks://to/sign"
+        document.title = this.title
+        console.log()
+        location.href = "qlinks://to/sign"
     },
-    methods:{
-        openView(url){
-          console.log("url",url)
-          location.href=url
-        }
-    }
-};
+    methods: {
+        openView(url) {
+            console.log("url", url)
+            location.href = url
+        },
+    },
+}
 </script>
 <style>
 .rich_media_title {
-  font-size: 22px;
-  line-height: 1.4;
-  margin-bottom: 14px;
-  font-weight: 400;
-  margin-left: 20px;
-  margin-right: 20px;
+    font-size: 22px;
+    line-height: 1.4;
+    margin-bottom: 14px;
+    font-weight: 400;
+    margin-left: 20px;
+    margin-right: 20px;
 }
 .rich_media_subtitle {
-  display: inline-block;
-  vertical-align: middle;
-  font-size: 15px;
-  -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
-  margin-left: 20px;
-  margin-right: 20px;
-  color: rgba(0, 0, 0, 0.3);
+    display: inline-block;
+    vertical-align: middle;
+    font-size: 15px;
+    -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+    margin-left: 20px;
+    margin-right: 20px;
+    color: rgba(0, 0, 0, 0.3);
 }
 h2 {
-  display: block;
-  font-size: 1.5em;
-  margin-block-start: 0.83em;
-  margin-block-end: 0.83em;
-  margin-inline-start: 0px;
-  margin-inline-end: 0px;
-  font-weight: bold;
+    display: block;
+    font-size: 1.5em;
+    margin-block-start: 0.83em;
+    margin-block-end: 0.83em;
+    margin-inline-start: 0px;
+    margin-inline-end: 0px;
+    font-weight: bold;
 }
 </style>
